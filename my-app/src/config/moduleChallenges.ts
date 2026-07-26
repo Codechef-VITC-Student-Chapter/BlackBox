1790a38747dbe2bbadb8c57c1d919e99204b6f04
manmay2 <imanmay2@gmail.com>
2026-07-26 22:39:11 +0530

    Update recovery key for repository challenge

--- my-app/src/config/moduleChallenges.ts
+++ my-app/src/config/moduleChallenges.ts
@@ -2,7 +2,7 @@ export const REPOSITORY_RECOVERY_CHALLENGE = {
   moduleNumber: 2,
   owner: "ishani2025",
   repository: "bLaCKbOXX_C_C",
-  recoveryKey: "imanmay2",
+  recoveryKey: "7HBQ3RXM9KT2WV5NPZ4YJ8DA",
   nextModule: 3,
   nextRoute: "/network-labyrinth",
   futureChallengeData: {},
